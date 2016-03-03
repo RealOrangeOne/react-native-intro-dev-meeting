b79ff7736598fd5ff98b4053ab52b3ed3792fd59
Jake Howard <git@theorangeone.net>
2016-03-03 19:13:39 +0000

    Init Reveal

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import Reveal from 'reveal.js'
+
+window.Reveal = Reveal;
+
+Reveal.initialize({
+				controls: true,
+				progress: true,
+				center: true,
+
+				transition: 'slide',
+
+				dependencies: [
+					{ src: 'js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+				]
+			});
