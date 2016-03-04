c521767134fe79d1bf41ab9d5ac1ac5ce5c3999e
RealOrangeOne <git@theorangeone.net>
2016-03-04 09:15:38 +0000

    Fix server linting

--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,10 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+const connect = require('connect');
+const serveStatic = require('serve-static');
 
 const SERVE_DIR = __dirname + '/../build';
 const PORT = 8070;
 
 
-connect().use(serveStatic(SERVE_DIR)).listen(PORT, function(){
-    console.log('Server running on', PORT, '...');
+connect().use(serveStatic(SERVE_DIR)).listen(PORT, function() {
+  console.log('Server running on', PORT, '...');
 });
