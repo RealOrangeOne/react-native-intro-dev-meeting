cb6a09ebc94e967e4d8515492695ff117cb75d16
RealOrangeOne <git@theorangeone.net>
2016-03-04 09:15:48 +0000

    Fix publish output dir

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo ">> Compressing JS..."
-uglifyjs build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o src/js/app.js
+uglifyjs build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o build/js/app.js
 
 echo ">> Compressing CSS..."
 cleancss -d --s0 -o build/css/style.css build/css/style.css
