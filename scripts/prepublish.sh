5b3189a713948f648cf6c66e0a19366173e05dc5
Jake Howard <git@theorangeone.net>
2018-01-21 19:40:24 +0000

    Stop deploying on gh-pages

--- scripts/prepublish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo ">> Compressing JS..."
-uglifyjs build/js/app.js --compress --screw-ie8 --define --stats --keep-fnames -o build/js/app.js
-
-echo ">> Compressing CSS..."
-cleancss -d --s0 -o build/css/style.css build/css/style.css
